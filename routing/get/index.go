7832e9f6da757ae18b59093e017b6aaeafeae900
masayoshi4649 <masayoshi4649@gmail.com>
2020-10-12 03:42:18 +0900

    2020/10/12 03:42

--- routing/get/index.go
+++ routing/get/index.go
@@ -1,8 +1,39 @@
 package get
 
-import "github.com/gin-gonic/gin"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+
+	"github.com/gin-gonic/gin"
+)
+
+// Content ... labels
+type Content struct {
+	Label1 string `json:"label1"`
+	Label2 string `json:"label2"`
+	Label3 string `json:"label3"`
+}
+
+// Lang ... languages
+type Lang struct {
+	Ja Content `json:"ja"`
+	En Content `json:"en"`
+}
+
+var lang Lang
 
 // Index ... GET index.html
 func Index(ctx *gin.Context) {
+
+	jsonFromFile, err := ioutil.ReadFile("./lang/index.json")
+	if err != nil {
+		panic(err)
+	}
+	json.Unmarshal(jsonFromFile, &lang)
+
+	fmt.Println(lang.Ja.Label1)
+	fmt.Println(lang.En.Label1)
+
 	ctx.HTML(200, "index.html", gin.H{})
 }
