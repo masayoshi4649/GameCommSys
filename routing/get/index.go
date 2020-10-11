f43ce9c82cac784650d47c8ec466ef6d8c54f6c7
masayoshi4649 <masayoshi4649@gmail.com>
2020-10-12 04:36:41 +0900

    2020年10月12日 4時36分 付け修正

--- routing/get/index.go
+++ routing/get/index.go
@@ -15,25 +15,39 @@ type Content struct {
 	Label3 string `json:"label3"`
 }
 
-// Lang ... languages
-type Lang struct {
+// LangData ... languages
+type LangData struct {
 	Ja Content `json:"ja"`
 	En Content `json:"en"`
 }
 
-var lang Lang
+var langData LangData
 
 // Index ... GET index.html
 func Index(ctx *gin.Context) {
+
+	// デバッグ用
+	var debuglang = "ja"
 
 	labeljson, err := ioutil.ReadFile("./lang/index.json")
 	if err != nil {
 		panic(err)
 	}
-	json.Unmarshal(labeljson, &lang)
+	json.Unmarshal(labeljson, &langData)
 
-	fmt.Println(lang.Ja.Label1)
-	fmt.Println(lang.En.Label1)
+	fmt.Println(langData.Ja.Label1)
+	fmt.Println(langData.En.Label1)
 
-	ctx.HTML(200, "index.html", gin.H{})
+	switchLang(debuglang, ctx, langData)
+}
+
+func switchLang(lang string, ctx *gin.Context, langData LangData) {
+	switch lang {
+	case "ja":
+		ctx.HTML(200, "index.html", gin.H{"label1": langData.Ja.Label1, "label2": langData.Ja.Label2, "label3": langData.Ja.Label3})
+	case "en":
+		ctx.HTML(200, "index.html", gin.H{"label1": langData.En.Label1, "label2": langData.En.Label2, "label3": langData.En.Label3})
+	default:
+		ctx.HTML(200, "index.html", gin.H{"label1": langData.En.Label1, "label2": langData.En.Label2, "label3": langData.En.Label3})
+	}
 }
